154f3ff07d253bdbee259b1d033e5298d06768c3
DrewMas <andrewmasnyi@gmail.com>
2021-09-06 20:56:37 +0300

    kolkhoznyi ratings done

--- src/components/stars/Stars.js
+++ src/components/stars/Stars.js
@@ -1,25 +1,27 @@
+import {RatingView} from 'react-simple-star-rating'
+
 import './Stars.css'
 
 
 export default function Stars({item:{vote_average}}) {
-    console.log(vote_average);
-
-    console.log(Math.round(vote_average));
-    let stars = [];
-
-   const countStars = () => {
-        for (let i = 0; i < Math.round(vote_average); i++) {
-            stars.push('★');
-        }
-        return stars;
-    }
+   //  console.log(vote_average);
+   //
+   //  console.log(Math.round(vote_average));
+   //  let stars = [];
+   //
+   // const countStars = () => {
+   //      for (let i = 0; i < Math.round(vote_average); i++) {
+   //          stars.push('★');
+   //      }
+   //      return stars;
+   //  }
 
 
     return (
         <div className={'ratingContainer'}>
             <div className={'ratingNUmber'}>{vote_average}/10</div>
-            <div className={'stars'}>{countStars()}</div>
-
+            {/*<div className={'stars'}>{countStars()}</div>*/}
+            <RatingView ratingValue={vote_average-0.5}  stars={10} size={16}/>
         </div>
     );
 }

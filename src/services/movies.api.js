39bb5820176c7f6793424b632b46c98a12a5a340
DrewMas <andrewmasnyi@gmail.com>
2021-09-09 17:47:46 +0300

    changed api service layout

--- src/services/movies.api.js
+++ src/services/movies.api.js
@@ -3,39 +3,19 @@ import axios from "axios";
 const token = `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNzE0ZDE2MmIyMGI0ZDkyOWZhNzU5OTk0NWNmMzI1YSIsInN1YiI6IjYxMmZiOTEwZTM4YmQ4MDA4M2NhODUyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lW-wtc_gCd43fh1KAGjnmruDvsDYXKtPQ_2F-GJUQ3A`;
 const baseUrl = 'https://api.themoviedb.org/3/';
 
-const movies = axios.create({
+const instance = axios.create({
     baseURL: baseUrl,
     headers: {
         Authorization: `Bearer ${token}`,
     },
 });
 
-const getMovies = async ()=> await movies.get(`/discover/movie`);
+const getMovies = async ()=> await instance.get(`/discover/movie`);
 
+const getMovieInfo = async (id) => await instance.get(`/movie/${id}`)
 
-const movieInfo = axios.create({
-    baseURL: baseUrl,
-    headers: {
-        Authorization: `Bearer ${token}`,
-    }
-});
-const getMovieInfo = async (id) => await movieInfo.get(`/movie/${id}`)
+const getPages = async (page) => await instance.get(`/discover/movie?api_key=${token}&page=${page}`)
 
-
-const pages = axios.create({
-    baseURL: baseUrl,
-    headers: {
-        Authorization: `Bearer ${token}`,
-    }
-});
-const getPages = async (page) => await pages.get(`/discover/movie?api_key=${token}&page=${page}`)
-
-const videos = axios.create({
-    baseURL: baseUrl,
-    headers: {
-        Authorization: `Bearer ${token}`
-    }
-})
-const getVideo = async (movie_id)=> await videos.get(`movie/${movie_id}/videos?api_key=${token}&language=en-US`)
+const getVideo = async (movie_id)=> await instance.get(`movie/${movie_id}/videos?api_key=${token}&language=en-US`)
 
 export {getMovies, getMovieInfo, getPages, getVideo}
